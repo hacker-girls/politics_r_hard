dc0efbee51367f517aca1e977be2eea2d5fbf235
Sonia Rao <soniahrao@gmail.com>
2018-10-21 04:36:34 -0400

    model->app.py

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ from flask import Flask, request, render_template
 from twitter import *
 import json
 import requests
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report, confusion_matrix
 
 OAUTH_TOKEN="2464717370-ztIheNqKFIr9ll1ZG3OEa1SxRPTGY8k1XL3Ukj0"
 OAUTH_SECRET="doEQPqBTLo22FrakNfY2q3jdLJyary6TFcLT8sv8AJes7"
@@ -17,6 +26,48 @@ twitter = Twitter (
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
+def model(user_df):
+        def csv_to_df(pathname,val):
+                df = pd.read_csv(pathname).drop(['Unnamed: 0'],axis=1)
+                df = df.sample(frac=1).reset_index(drop=True) #shuffle it up
+                df['pol'] = np.full((df.shape[0],1),val)
+                return df
+
+        leftc = csv_to_df('/data/left_cong.csv',1)
+        leftc = leftc.rename(columns = {'Sources':'News'})
+        left = pd.concat([csv_to_df('/data/left1.csv',1),leftc],ignore_index=True)
+        rightc = csv_to_df('/data/right_cong.csv',-1)
+        rightc = rightc.rename(columns = {'Sources':'News'})
+        right = pd.concat([csv_to_df('/data/left1.csv',-1),rightc],ignore_index=True)
+        center = csv_to_df('/data/center1.csv',0)
+
+        total_df = pd.concat([left,center,right],ignore_index=True)
+        total_df = total_df.sample(frac=1).reset_index(drop=True)
+        
+        stop_words = set(stopwords.words('english'))
+        
+        def pre_process(mess):
+                mess = nltk.word_tokenize(mess)
+                clean = [word.lower() for word in mess if word.lower() not in stop_words]
+                return clean
+        
+        def vocab_length(total):
+                sum_ = 0
+                for row in total:
+                        sum_ += len(row)
+                        return sum_
+                
+        def piped_vect(X,y,user_x):
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)    
+                text_clf = Pipeline([('vect', CountVectorizer(analyzer=pre_process,encoding='utf-8',strip_accents=['ascii','unicode'],max_df=0.8,min_df=0.3)),('tfidf',TfidfTransformer()),('clf', MultinomialNB()),])
+                text_clf = text_clf.fit(X_train, y_train)
+                predicted = text_clf.predict(user_x)
+                return predicted
+
+        pred = piped_vect(total_df['Tweets'],total_df['pol'],user_df['Tweets'])
+        print pred
+
+
 def form_input():
 	if request.method == 'POST':
 		screen_name = request.form['screen_name']
@@ -34,6 +85,10 @@ def form_input():
 	df = pd.DataFrame(data, columns=['Sources', 'Tweets'])
 	# app.logger.debug(itpTweets)
 
+        #DF IS THE IMPORTANT DATAFRAME
+        #DF->MODEL
+        pred = model(df)
+        
 	address = location
 
 	params = {
