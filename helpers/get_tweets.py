9a91de9c21693bd1b3f0671453d001197cf6c75e
ndellamaria <nrdell3@gmail.com>
2018-10-20 22:09:19 -0400

    helper

--- helpers/get_tweets.py
+++ helpers/get_tweets.py
@@ -1,8 +1,5 @@
 import twitter
-from flask import Flask, request, render_template
 from twitter import *
-import json
-import urllib.request
 import pandas as pd
 
 data = {'Sources': [], 'Tweets': []}
@@ -16,42 +13,22 @@ twitter = Twitter (
 	auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 ) 
 
-page_link = 'https://ballotpedia.org/List_of_current_members_of_the_U.S._Congress'
-page_response = requests.get(page_link, timeout=5)
-page_content = BeautifulSoup(page_response.content, "html.parser")
+def get_tweets_db(name):		
+	result_accts = twitter.users.search(q = name)
+	user_found = False
 
-tables = page_content.find_all('table', {'class':'wikitable sortable jquery-tablesorter'})
-for table in tables:
-	rows = table.find_all('tr')
-	for i in range(1, len(rows)):
-		elements = rows[i].find_all('td')
-		name = elements[0].getText().strip('\n')
-		party = elements[3].getText().strip('\n\t\t\t\t\t')
-		
-		result_accts = twitter.users.search(q = name)
+	for acct in result_accts:
+		if acct["verified"] == True:
+			user_found = True
+			data['Sources'] = name
+			tlist = []
+			for t in twitter.statuses.user_timeline(count=2000, screen_name=acct["screen_name"]):
+				tlist.append(t['text'])
+			data['Tweets'].append(tlist)
+			break
 
-		try:
-			acct = result_accts[0]
-			if acct["verified"] == True:
-				if party == 'Democratic Party' or party == 'Independent':
-					ldata['Sources'].append(name)
-					tlist = []
-					for t in twitter.statuses.user_timeline(count=2000, screen_name=acct["screen_name"]):
-						tlist.append(t['text'])
-					ldata['Tweets'].append(tlist)
-				else:
-					rdata['Sources'].append(name)
-					tlist = []
-					for t in twitter.statuses.user_timeline(count=2000, screen_name=acct["screen_name"]):
-						tlist.append(t['text'])
-					rdata['Tweets'].append(tlist)
+	if user_found:
+		df = pd.DataFrame(data, columns=['Sources', 'Tweets'])
+		return df
 
-		except IndexError:
-			pass
-
-ldf = pd.DataFrame(ldata, columns=['Sources', 'Tweets'])
-ldf.to_csv('left_cong_tweets.csv')
-
-rdf = pd.DataFrame(rdata, columns=['Sources', 'Tweets'])
-rdf.to_csv('right_cong_tweets.csv')
-
+	return False
